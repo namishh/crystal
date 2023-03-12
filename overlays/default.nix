0c407f5b7f6171a24e7d82afb3b2539ab077b994
chadcat5207 <namishpande2007@gmail.com>
2023-03-12 16:47:30 +0530

    hi

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,7 @@
 {
   additions = final: _prev: import ../pkgs { pkgs = final; inherit inputs; };
   modifications = final: prev: {
-    st = (st.overrideAttrs (oldAttrs: rec {
+    st = prev.st.overrideAttrs (oldAttrs: rec {
       buildInputs = oldAttrs.buildInputs ++ [ harfbuzz ];
       src = fetchFromGitHub {
         owner = "chadcat7";
@@ -10,7 +10,7 @@
         rev = "b4e13bd68a4dd6e89d691c7a0721ae4c8ffc4908";
         sha256 = "0mjba15phjm4rnwz6ni6731zqw3j3wb6w9pvw0nv8ikj4ncih8r6";
       };
-    }));
+    });
     aster = prev.callPackage ../derivs/aster.nix { };
   };
 }
