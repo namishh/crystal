318fca0dcce81cb88e49b1d67c248fd85e33ae22
chadcat5207 <namishpande2007@gmail.com>
2023-03-12 17:07:27 +0530

    rst

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,15 +2,15 @@
 {
   additions = final: _prev: import ../pkgs { pkgs = final; inherit inputs; };
   modifications = final: prev: {
-    #aster = prev.callPackage ../derivs/aster.nix { };
-    st = prev.st.overrideAttrs (oldAttrs: {
-      buildInputs = oldAttrs.buildInputs ++ [ prev.harfbuzz ];
-      src = prev.fetchFromGitHub {
-        owner = "chadcat7";
-        repo = "best";
-        rev = "b4e13bd68a4dd6e89d691c7a0721ae4c8ffc4908";
-        sha256 = "0mjba15phjm4rnwz6ni6731zqw3j3wb6w9pvw0nv8ikj4ncih8r6";
-      };
-    });
+    aster = prev.callPackage ../derivs/aster.nix { };
+    #st = prev.st.overrideAttrs (oldAttrs: {
+    #  buildInputs = oldAttrs.buildInputs ++ [ prev.harfbuzz ];
+    #  src = prev.fetchFromGitHub {
+    #    owner = "chadcat7";
+    #    repo = "best";
+    #    rev = "b4e13bd68a4dd6e89d691c7a0721ae4c8ffc4908";
+    #    sha256 = "0mjba15phjm4rnwz6ni6731zqw3j3wb6w9pvw0nv8ikj4ncih8r6";
+    #  };
+    #});
   };
 }
