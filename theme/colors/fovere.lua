17273aa8c37c1e252196572180cda11b518efb20
Namish <namishpande2007@gmail.com>
2024-01-04 13:19:50 +0530

    fovere

---
+++ theme/colors/fovere.lua
@@ -0,0 +1,17 @@
+local M = {}
+M.type  = "dark"
+M.name  = 'fovere'
+M.pri   = "#c297d5"
+M.warn  = "#fcc48c"
+M.err   = "#cf5555"
+M.ok    = "#6dc383"
+M.dis   = "#a7b3fb"
+M.bg    = "#111012"
+M.mbg   = "#1d1a1f"
+M.bg3   = "#221d23"
+M.bg4   = "#252027"
+M.fg2   = "#e6e6e6"
+M.fg    = "#dadada"
+M.fg3   = "#c9c9c9"
+M.fg4   = "#6f6f6f"
+return M
